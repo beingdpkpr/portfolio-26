99cab67797ebd2fb3758ccd4ce7212aec329354d
Deepak Prasad <dpkpr@outlook.com>
2026-04-23 22:46:16 +0530

    chore: add Vitest + React Testing Library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/deepak-prasad/',
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['src/test/setup.ts'],
+    globals: true,
+  },
 })
